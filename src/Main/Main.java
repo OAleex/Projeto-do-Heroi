5fe996466aa9a869149f650e1a2162e0807e2fa2
ivandersonpy <ivandersonamaral16@gmail.com>
2022-12-05 15:22:00 -0300

    implemento do while

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -12,7 +12,7 @@ import Categorias.Paladino;
 
 
 
-public class Main extends CategoriaDoPersonagem implements Ataques {
+public class Main extends CategoriaDoPersonagem { //implements Ataques {
 
     public Main(String categoriaDoPersonagem) {
         super(categoriaDoPersonagem);
@@ -32,50 +32,59 @@ public class Main extends CategoriaDoPersonagem implements Ataques {
         // Tratamento de erro e Scanner
 
         try {
-            Scanner selecionarCategoria = new Scanner(System.in);
-            int selecionar = selecionarCategoria.nextInt();
 
-            switch (selecionar) {
-                case 1:
-                    System.out.println("Voce selecionou o Guerreiro");
-                    Guerreiro guerreiro = new Guerreiro(Guerreiro.addArma);
-                    System.out.println(guerreiro);
-                    guerreiro.ataqueDoPersonagem();
-                    break;
-                case 2:
-                    System.out.println("Voce selecionou o Mago");
-                    Mago mago = new Mago(Mago.addArma);
-                    System.out.println(mago);
-                    mago.ataqueDoPersonagem();
-                    break;
-                case 3:
-                    System.out.println("Voce selecionou o Paladino");
-                    Paladino paladino = new Paladino(Paladino.addArma);
-                    System.out.println(paladino);
-                    paladino.ataqueDoPersonagem();
-                    break;
-                case 4:
-                    System.out.println("Voce selecionou o Orc");
-                    Orc orc = new Orc(Orc.addArma);
-                    System.out.println(orc);
-                    orc.ataqueDoPersonagem();
-                    break;
-                case 5:
-                    System.out.println("Voce selecionou o Espadachim");
-                    Espadachim espadachim = new Espadachim(Espadachim.addArma);
-                    System.out.println(espadachim);
-                    espadachim.ataqueDoPersonagem();
-                    break;
-                default:
-                    System.out.println("Opcao invalida.");
+
+            int opcao = 0;
+
+            while (opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4 && opcao != 5) {
+
+                Scanner selecionarCategoria = new Scanner(System.in);
+                opcao = selecionarCategoria.nextInt();
+
+                switch (opcao) {
+                    case 1:
+                        System.out.println("Voce selecionou o Guerreiro");
+                        Guerreiro guerreiro = new Guerreiro(Guerreiro.addArma);
+                        System.out.println(guerreiro);
+                        guerreiro.ataqueDoPersonagem();
+                        break;
+                    case 2:
+                        System.out.println("Voce selecionou o Mago");
+                        Mago mago = new Mago(Mago.addArma);
+                        System.out.println(mago);
+                        mago.ataqueDoPersonagem();
+                        break;
+                    case 3:
+                        System.out.println("Voce selecionou o Paladino");
+                        Paladino paladino = new Paladino(Paladino.addArma);
+                        System.out.println(paladino);
+                        paladino.ataqueDoPersonagem();
+                        break;
+                    case 4:
+                        System.out.println("Voce selecionou o Orc");
+                        Orc orc = new Orc(Orc.addArma);
+                        System.out.println(orc);
+                        orc.ataqueDoPersonagem();
+                        break;
+                    case 5:
+                        System.out.println("Voce selecionou o Espadachim");
+                        Espadachim espadachim = new Espadachim(Espadachim.addArma);
+                        System.out.println(espadachim);
+                        espadachim.ataqueDoPersonagem();
+                        break;
+                    default:
+                        System.out.println("Opcao invalida.\nSelecione uma Categoria para seu guerreiro:");
+                }
             }
         } catch (InputMismatchException e) {
-            System.out.println("Nao pode digitar Strings");
+            System.out.println("Digite apenas numeros.");
         }
     }
-
-    @Override
-    public void ataqueDoPersonagem() {
-
-    }
 }
+
+//
+//    @Override
+//    public void ataqueDoPersonagem() {
+//
+//    }
+//}
